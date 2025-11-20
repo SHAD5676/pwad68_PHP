5d510efb9e9c54009adcaa22851ed661b623dd52
Razib Hasan <razibhasan2403@gmail.com>
2025-11-20 13:00:22 +0600

    20 nov, cw

---
+++ php_project/AdminLTE-3.2.0/admin/includes/db_config.php
@@ -0,0 +1,21 @@
+ <?php 
+ 
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "pwad68";
+
+$db = new mysqli($host, $user, $password, $database);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+
+    
+}
+//echo "Connected successfully";
+
+$project_url = "http://localhost/pwad68/phpproject/admin/";
+
+//$db->close();
+
+ ?>
