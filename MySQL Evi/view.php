0ff6e7b2803208170e6dfdea19e56f20b317df59
Razib Hasan <razibhasan2403@gmail.com>
2025-11-27 12:50:51 +0600

    27 nov

---
+++ MySQL Evi/view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Product List</h2>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Product Name</th>
+            <th>Manufacturer Name</th>
+            <th>Price</th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+
+</html>
